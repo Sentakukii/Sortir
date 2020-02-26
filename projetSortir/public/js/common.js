4d37cb923ccd1aa716748fdbf9c01bb3b31c1018
MaelSEVERE <mael.severe2018@campus-eni.fr>
2020-02-26 15:17:07 +0100

    fix input file

--- projetSortir/public/js/common.js
+++ projetSortir/public/js/common.js
@@ -24,3 +24,19 @@ if (typeof App.Flash === typeof undefined) {
             }, {});
     };
 }
+
+$(document).ready(function() {
+    if (document.querySelector('input[type="file"]')) {
+        document.querySelector('input[type="file"]').addEventListener("change", function () {
+            var fullPath = document.querySelector('input[type="file"]').value;
+            if (fullPath) {
+                var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+                var filename = fullPath.substring(startIndex);
+                if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+                    filename = filename.substring(1);
+                }
+                document.querySelector('input[type="file"] + label').textContent = filename;
+            }
+        });
+    }
+});
