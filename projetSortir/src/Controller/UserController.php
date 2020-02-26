ab9fd18a0b21e1e8e05ad894734a86d5f0b753f9
MaelSEVERE <mael.severe2018@campus-eni.fr>
2020-02-26 11:14:39 +0100

    all users

---
+++ projetSortir/src/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="user")
+     */
+    public function index(EntityManagerInterface $em)
+    {
+        $users = $em->getRepository(User::class)->findAll();
+
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+            'users' => $users,
+        ]);
+    }
+}
